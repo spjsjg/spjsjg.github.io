47a0b412ac6ddeb931066d755b0ba89a474b7263
spjsjg <chengsanping123@outlook.com>
2020-02-02 19:07:12 +0800

    2020-02-01

--- js/gallery_images.js
+++ js/gallery_images.js
@@ -1,11 +1,7 @@
-var contents;
-
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		deal_with(this);
-		document.write(contents);
-
 	}
 };
 xhttp.open("POST", "/images-2020.txt", true);
@@ -53,6 +49,6 @@ function deal_with(xml) {
 +					'</a>'
 +				'</div>'
 +			'</div>'
-		document.write(contents)
+		document.write(str[i])
 	};
 }
