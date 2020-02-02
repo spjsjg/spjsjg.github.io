d288ccbad36d14022b57e32ecde6e01b8e830121
spjsjg <chengsanping123@outlook.com>
2020-02-02 20:11:24 +0800

    2020-02-01

--- js/gallery_images.js
+++ js/gallery_images.js
@@ -4,12 +4,12 @@ var imgurl3 = "https://coding.net/u/c2726139513/p/images-2020/git/raw/master/"
 var imgurl4_1 = "https://dev.azure.com/chengsanping123/6a5f0f3a-b511-4496-97c1-5225a1f263c2/_apis/git/repositories/73e4682c-679f-49eb-998a-379a678c4c2d/items?path=%2F"
 var imgurl4_2 = "&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0"
 var imgurl5 = "images/"
-var str=new Array();
-var str1=new Array();
-var str2=new Array();
-var month=new Array();
-var name=new Array();
-var str3=
+var str = new Array();
+var str1 = new Array();
+var str2 = new Array();
+var month = new Array();
+var name = new Array();
+var str3 = 
 	'<div class="zoom-container">'
 	+	'<div class="zoom-caption">'
 	+		'<div class="caption-inner">'
@@ -48,7 +48,7 @@ function deal_with(xml) {
 +						'</div>'
 +					'</a>'
 +				'</div>'
-+			'</div>'
++			'</div>';
 		document.write(str[i])
 	};
 }
