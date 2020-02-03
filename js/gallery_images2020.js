6e15de71fa245bc0a5103be0f3afde10e31e9783
spjsjg <chengsanping123@outlook.com>
2020-02-03 11:55:53 +0800

    2020-02-03

--- js/gallery_images2020.js
+++ js/gallery_images2020.js
@@ -1,17 +1,17 @@
-var imgurl1 = "https://gitee.com/spjsjg/images-2020/raw/master/"
-var imgurl2 = "https://raw.githubusercontent.com/spjsjg/images-2020/master/"
-var imgurl3 = "https://coding.net/u/c2726139513/p/images-2020/git/raw/master/"
-var imgurl4_1 = "https://dev.azure.com/chengsanping123/6a5f0f3a-b511-4496-97c1-5225a1f263c2/_apis/git/repositories/73e4682c-679f-49eb-998a-379a678c4c2d/items?path=%2F"
-var imgurl4_2 = "&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0"
-var imgurl5 = "images/"
-var str = new Array();
-var str1 = new Array();
-var str2 = new Array();
-var mon = new Array();
-var monzh = new Array();
-var nm = new Array();
-var year = "2020年"
-var str3 = 
+var imgurl2020_1 = "https://gitee.com/spjsjg/images-2020/raw/master/"
+var imgurl2020_2 = "https://raw.githubusercontent.com/spjsjg/images-2020/master/"
+var imgurl2020_3 = "https://coding.net/u/c2726139513/p/images-2020/git/raw/master/"
+var imgurl2020_4_1 = "https://dev.azure.com/chengsanping123/6a5f0f3a-b511-4496-97c1-5225a1f263c2/_apis/git/repositories/73e4682c-679f-49eb-998a-379a678c4c2d/items?path=%2F"
+var imgurl2020_4_2 = "&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=octetStream&api-version=5.0"
+var imgurl2020_5 = "images/"
+var str2020 = new Array();
+var str2020_1 = new Array();
+var str2020_2 = new Array();
+var mon2020 = new Array();
+var monzh2020 = new Array();
+var nm2020 = new Array();
+var year2020 = "2020年"
+var str2020_3 = 
 	'<div class="zoom-container">'
 	+	'<div class="zoom-caption">'
 	+		'<div class="caption-inner">'
@@ -21,14 +21,14 @@ var str3 =
 	+		'</div>'
 	+	'</div>';
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
+var xhttp2020 = new XMLHttpRequest();
+xhttp2020.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 		deal_with(this);
 	}
 };
-xhttp.open("GET", "images-2020.txt", true);
-xhttp.send();
+xhttp2020.open("GET", "images-2020.txt", true);
+xhttp2020.send();
 
 function deal_with(xml) {
 	contents = xml.responseText;
@@ -37,66 +37,66 @@ function deal_with(xml) {
 	for (var i=0; i<line.length-1; i++)
 	{
 		var data = line[i].split(" ")
-		mon[i] = data[0]
-		nm[i] = data[2]
-		str1[i]='<a class="example-image-link" href="'+imgurl4_1+'images%2F'+nm[i]+imgurl4_2+'" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">'
-		str2[i]='<img class="example-image" src="'+imgurl4_1+'images%2Fthumb%2F'+nm[i]+imgurl4_2+'" alt=""/>'
-//		str1[i]='<a class="example-image-link" href="'+imgurl5+'index/'+name[i]+'" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">'
-//		str2[i]='<img class="example-image" src="'+imgurl5+'index/thumb/'+name[i]+'" alt=""/>'
-		str[i]=	
+		mon2020[i] = data[0]
+		nm2020[i] = data[2]
+		str2020_1[i]='<a class="example-image-link" href="'+imgurl2020_4_1+'images%2F'+nm2020[i]+imgurl2020_4_2+'" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">'
+		str2020_2[i]='<img class="example-image" src="'+imgurl2020_4_1+'images%2Fthumb%2F'+nm2020[i]+imgurl2020_4_2+'" alt=""/>'
+//		str2020_1[i]='<a class="example-image-link" href="'+imgurl2020_5+'index/'+nm2020[i]+'" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">'
+//		str2020_2[i]='<img class="example-image" src="'+imgurl2020_5+'index/thumb/'+nm2020[i]+'" alt=""/>'
+		str2020[i]=	
 			'<div class="col-1-6">'
 		+		'<div class="wrap-col">'
 		+			'<div class="item">'
-		+	str1[i]+str3+str2[i]
+		+	str2020_1[i]+str2020_3+str2020_2[i]
 		+					'</div>'
 		+				'</a>'
 		+			'</div>'
 		+		'</div>'
 		+	'</div>';
-		switch(mon[i])
+		switch(mon2020[i])
 		{
 			case "Jan":
-				monzh[i]="1月";
+				monzh2020[i]="1月";
 				break;
 			case "Feb":
-				monzh[i]="2月";
+				monzh2020[i]="2月";
 				break;
 			case "Mar":
-				monzh[i]="3月";
+				monzh2020[i]="3月";
 				break;
 			case "Apr":
-				monzh[i]="4月";
+				monzh2020[i]="4月";
 				break;
 			case "May":
-				monzh[i]="5月";
+				monzh2020[i]="5月";
 				break;
 			case "Jun":
-				monzh[i]="6月";
+				monzh2020[i]="6月";
 				break;
 			case "Jul":
-				monzh[i]="7月";
+				monzh2020[i]="7月";
 				break;
 			case "Aug":
-				monzh[i]="8月";
+				monzh2020[i]="8月";
 				break;
 			case "Sept":
-				monzh[i]="9月";
+				monzh2020[i]="9月";
 				break;
 			case "Oct":
-				monzh[i]="10月";
+				monzh2020[i]="10月";
 				break;
 			case "Nov":
-				monzh[i]="11月";
+				monzh2020[i]="11月";
 				break;
 			case "Dec":
-				monzh[i]="12月";
+				monzh2020[i]="12月";
 				break;
 		}
-		if (mon[i] != mon[i-1])
+		if (mon2020[i] != mon2020[i-1])
 		{
-			gl += '<p>'+year+monzh[i]+'</p>'
+			gl += '<p>'+year2020+monzh2020[i]+'</p>'
 		}	
-		gl += str[i];
+		gl += str2020[i];
 	};
-	document.getElementById("gallery").innerHTML = gl
+	document.getElementById("gallery2020").innerHTML = gl
 }
